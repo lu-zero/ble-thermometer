1a8b269d4750801f3b37313afe830256ae26eafe
Luca Barbato <lu_zero@gentoo.org>
2024-01-14 06:40:04 +0100

    Save values using gluesql

--- src/bin/ble-thermo.rs
+++ src/bin/ble-thermo.rs
@@ -3,7 +3,10 @@
 
 use ble_thermometers::Scanner;
 use futures::future;
-use futures::stream::StreamExt;
+use futures::stream::{StreamExt, TryStreamExt};
+use gluesql::core::ast_builder::{self, num, text, timestamp, Execute};
+use gluesql::json_storage::JsonStorage;
+use gluesql::prelude::Glue;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -13,12 +16,39 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let stream = scanner.stream().await?;
 
+    let storage = JsonStorage::new("logs/")?;
+
+    let mut glue = Glue::new(storage.clone());
+
+    ast_builder::table("temps")
+        .create_table_if_not_exists()
+        .add_column("name TEXT")
+        .add_column("ts TIMESTAMP")
+        .add_column("temp FLOAT")
+        .add_column("humi FLOAT")
+        .execute(&mut glue)
+        .await?;
+
     stream
-        .for_each(|r| {
+        .then(|r| {
             println!("{:?}", r);
-            future::ready(())
+            let v = vec![
+                text(r.name.clone()),
+                timestamp(r.date.to_string()),
+                num(r.temp),
+                num(r.humi),
+            ];
+            let mut glue = Glue::new(storage.clone());
+            async move {
+                ast_builder::table("temps")
+                    .insert()
+                    .values(vec![v])
+                    .execute(&mut glue)
+                    .await
+            }
         })
-        .await;
+        .try_for_each(|_v| future::ready(Ok(())))
+        .await?;
 
     Ok(())
 }
